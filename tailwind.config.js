a5b114f4a3367810d2e21fd6d5c3d20b6fd7fac0
Samatar <thewind07@protonmail.com>
2020-04-07 22:43:35 -0400

    custon css

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        "gray-900-dashboard": "#0e0f0f",
+        "gray-800-dashboard": "#1d1e21",
+        "gray-700-dashboard": "#2E2E2E",
+        "gray-hover-sidebar": "#323232",
+        "gray-hover-light": "#D6D6D6"
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};
