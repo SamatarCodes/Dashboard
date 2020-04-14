4a286240fe405846d366dcb6697cb9be78a59480
Samatar <thewind07@protonmail.com>
2020-04-14 01:15:20 -0400

    nightly push - added content main header

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,10 @@ module.exports = {
         "gray-800-dashboard": "#1d1e21",
         "gray-700-dashboard": "#2E2E2E",
         "gray-hover-sidebar": "#323232",
-        "gray-hover-light": "#D6D6D6"
+        "gray-hover-light": "#D6D6D6",
+        "dark-color-sidebar": "#141517",
+        "dark-color-content": "#161719",
+        "gray-900-insidecontent": "#282A2E"
       }
     }
   },
