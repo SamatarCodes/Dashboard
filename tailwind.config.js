b42d5e9c06efd029fefe83cb6d680374495cb2da
Samatar <thewind07@protonmail.com>
2020-04-17 00:20:58 -0400

    nightly push - change sidebar and content color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "gray-900-dashboard": "#0e0f0f",
-        "gray-800-dashboard": "#1d1e21",
+        "gray-900-dashboard": "#202225",
+        "gray-800-dashboard": "#27292C",
         "gray-700-dashboard": "#2E2E2E",
         "gray-hover-sidebar": "#323232",
         "gray-hover-light": "#D6D6D6"
