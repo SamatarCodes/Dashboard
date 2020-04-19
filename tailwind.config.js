4c6498b83b8f1d4699e410b14aedabb0f095cb98
Samatar <thewind07@protonmail.com>
2020-04-19 01:06:19 -0400

    nightly push -  added Roboto font & changed bg colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "gray-900-dashboard": "#0e0f0f",
-        "gray-800-dashboard": "#1d1e21",
+        "gray-900-dashboard": "#2A2E32",
+        "gray-800-dashboard": "#212529",
         "gray-700-dashboard": "#2E2E2E",
         "gray-hover-sidebar": "#323232",
         "gray-hover-light": "#D6D6D6",
-        "dark-color-sidebar": "#141517",
-        "dark-color-content": "#161719",
+        "dark-color-sidebar": "#13151A",
+        "dark-color-content": "#1E2129",
         "gray-900-insidecontent": "#282A2E"
+      },
+
+      fontFamily: {
+        'roboto': ['Roboto Mono'],
       }
     }
   },
