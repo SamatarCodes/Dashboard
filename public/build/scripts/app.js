578d984c414a1d3f424ccf3ffd28ba144259865f
Samatar Code <samatarcode@Samatars-MacBook-Pro.local>
2020-04-21 18:57:51 -0400

    cleaned up the code for the dropdown menu

--- public/build/scripts/app.js
+++ public/build/scripts/app.js
@@ -1,12 +1,18 @@
+const profileImage = document.querySelector('.profile-image');
 const dropdown = document.querySelector('#profile-menu');
 const contentSection = document.querySelector('.content-dashboard');
 
-window.addEventListener('click', (e) => {
+document.addEventListener('click', (e) => {
+  // if element that was clicked on contained inside of dropdown
   console.log(e.target);
-  if (e.target === document.querySelector('.profile-image')) {
+  // if profile image is clicked
+  if (e.target === profileImage) {
     console.log('image');
+    // then show the dropdown menu
     dropdown.classList.toggle('show');
   } else {
+    // else close the dropdown menu if clicked outside
+    console.log(e.target);
     dropdown.classList.add('hide');
     dropdown.classList.remove('show');
   }
