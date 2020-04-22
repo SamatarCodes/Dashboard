5e0ef9a42da9380bcb5c1a09d61634e52aea102a
SamatarCodes <thewind07@protonmail.com>
2020-04-21 23:24:21 -0400

    changed sidebar fonts

--- public/build/scripts/app.js
+++ public/build/scripts/app.js
@@ -2,16 +2,12 @@ const profileImage = document.querySelector('.profile-image');
 const dropdown = document.querySelector('#profile-menu');
 
 document.addEventListener('click', (e) => {
-  // if element that was clicked on contained inside of dropdown
-  console.log(e.target);
   // if profile image is clicked
   if (e.target === profileImage) {
-    console.log('image');
     // then show the dropdown menu
     dropdown.classList.toggle('show');
   } else {
     // else close the dropdown menu if clicked outside
-    console.log(e.target);
     dropdown.classList.add('hide');
     dropdown.classList.remove('show');
   }
