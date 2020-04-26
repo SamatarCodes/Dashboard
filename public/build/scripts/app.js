9f9755a2e23e52a5ed55086a06878cffdf32c04c
SamatarCodes <thewind07@protonmail.com>
2020-04-25 22:03:21 -0400

    added hamburger menu

--- public/build/scripts/app.js
+++ public/build/scripts/app.js
@@ -1,5 +1,7 @@
 const profileImage = document.querySelector('.profile-image');
 const dropdown = document.querySelector('#profile-menu');
+const menuButton = document.querySelector('#menu-button');
+const searchField = document.querySelector('#search');
 
 document.addEventListener('click', (e) => {
   // if profile image is clicked
@@ -10,5 +12,9 @@ document.addEventListener('click', (e) => {
     // else close the dropdown menu if clicked outside
     dropdown.classList.add('hide');
     dropdown.classList.remove('show');
+  }
+
+  if (e.target === menuButton) {
+    searchField.classList.toggle('show');
   }
 });
